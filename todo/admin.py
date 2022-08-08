7b6feab840b634ee18fb41e340ce41536264aead
vanderpatrick <patrick123321123@icloud.com>
2022-08-08 15:43:47 +0000

    Register models in admin.py, create first database table, set up function to render table items in html page and html content to be render

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,6 @@
+# imports for admin.py
 from django.contrib import admin
 
+from .models import Item
 # Register your models here.
+admin.site.register(Item)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,13 @@
+"""Models for database """
+
 from django.db import models
 
 # Create your models here.
+
+""" class item first table from database """
+class Item(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):
+        return self.name
