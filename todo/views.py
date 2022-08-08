1f96f7ae8b050801e2002934dd7af218231274cc
vanderpatrick <patrick123321123@icloud.com>
2022-08-08 17:13:39 +0000

    Add create functionality to create a new toDo task

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 """ View section rendering functions using the database"""
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Item
 
 
@@ -10,3 +10,11 @@ def get_todo_list(request):
         'items': items
     }
     return render(request, 'todo/todo_list.html', context)
+
+def add_item(request):
+    if request.method == 'POST':
+        name = request.POST.get('item_name')
+        done = 'done' in request.POST
+        Item.objects.create(name=name, done=done)
+        return redirect('get_todo_list')
+    return render(request, 'todo/add_item.html')
